2bc4e0db3915f46289f4d05466d41404f7474296
hyc2017 <15071350256@163.com>
2018-02-27 00:05:52 +0800

    Update searchengine.py

--- chapter-4/searchengine.py
+++ chapter-4/searchengine.py
@@ -1,40 +1,74 @@
 import urllib.request as request
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import sqlite3
+import re
 
 ignorewords = set(['the','of','to','and','a','in','is','it'])
 
 class crawler:
-    def __init__(self,dbname):
-        pass
+    def __init__(self, dbname):
+        self.con = sqlite3.connect(dbname)
 
     def __del__(self):
-        pass
+        self.con.close()
 
     def dbcommit(self):
-        pass
+        self.con.commit()
 
-    def getentryid(self,table,field,value,createnew = True):
-        return None
+    def getentryid(self, table, field, value, createnew = True):
+        cur = self.con.execute("select rowid from %s where %s = '%s'" % (table, field, value))
+        res = cur.fetchone()
+        if res == None:
+            cur = self.con.execute("insert into %s (%s) values ('%s')" % (table, field, value))
+            return cur.lastrowid
+        else:
+            return res[0]
 
-    def addtoindex(self,url,soup):
+    def addtoindex(self, url, soup):
+        if self.isindexed(url): return
         print('indexing %s' % url)
 
-    def gettextonly(self,soup):
-        return None
+        text = self.gettextonly(soup)
+        words = self.separatewords(text)
 
-    def separatewords(self,text):
-        return None
+        urlid = self.getentryid('urllist', 'url', url)
 
-    def isindexed(self,url):
+        for i in range(len(words)):
+            word = words[i]
+            if word in ignorewords: continue
+            wordid = self.getentryid('wordlist', 'word', word)
+            self.con.execute('insert into wordlocation(urlid, wordid, location) values (%d, %d, %d)' % (urlid, wordid, i))
+
+    def gettextonly(self, soup):
+        v = soup.string
+        if v == None:
+            c = soup.contents
+            resulttext = ''
+            for t in c:
+                subtext = self.gettextonly(t)
+                resulttext += subtext + '\n'
+            return resulttext
+        else:
+            return v.strip()
+
+    def separatewords(self, text):
+        splitter = re.compile('\\W+')
+        return [s.lower() for s in  splitter.split(text) if s != '']
+
+    def isindexed(self, url):
+        u = self.con.execute("select rowid from urllist where url = '%s'" % url).fetchone()
+        if u != None:
+            v = self.con.execute('select * from wordlocation where urlid = %d' % u[0]).fetchone()
+            if v != None: return True
         return False
 
-    def addlinkhref(self,urlFrom,urlTo,linkText):
+    def addlinkhref(self, urlFrom, urlTo, linkText):
         pass
 
-    def crawl(self,pages,depth=2):
+    def crawl(self, pages, depth = 2):
         for i in range(depth):
-            newpages={}
+            newpages = set()
             for page in pages:
                 try:
                     c = request.urlopen(page)
@@ -42,23 +76,32 @@ class crawler:
                     print("Could not open %s" % page)
                     continue
                 try:
-                    soup = BeautifulSoup(c.read(),'lxml')
-                    self.addtoindex(page,soup)
+                    soup = BeautifulSoup(c.read(), 'lxml')
+                    self.addtoindex(page, soup)
                     links = soup('a')
                     for link in links:
                         if ('href' in dict(link.attrs)):
-                            url=urljoin(page,link['href'])
-                            if url.find("'")!=-1: continue
-                            url=url.split('#')[0]  # remove location portion
-                            if url[0:4]=='http' and not self.isindexed(url):
-                                newpages[url]=1
-                            #linkText=self.gettextonly(link)
-                            #self.addlinkref(page,url,linkText)
-                    #self.dbcommit()
-                    
+                            url = urljoin(page, link['href'])
+                            if url.find("'") != -1 : continue
+                            url = url.split('#')[0]  # remove location portion
+                            if url[0:4] == 'http' and not self.isindexed(url):
+                                newpages.add(url)
+                            linkText = self.gettextonly(link)
+                            self.addlinkhref(page,url,linkText)
+                    self.dbcommit()
                 except:
                     print("Could not parse page %s" % page)
-            pages=newpages
+            pages = newpages
 
     def createindextables(self):
-        pass
+        self.con.execute('create table urllist(url)')
+        self.con.execute('create table wordlist(word)')
+        self.con.execute('create table wordlocation(urlid, wordid, location)')
+        self.con.execute('create table link(fromid integer, toid integer)')
+        self.con.execute('create table linkwords(wordid, linkid)')
+        self.con.execute('create index urlidx on urllist(url)')
+        self.con.execute('create index wordidx on wordlist(word)')
+        self.con.execute('create index wordurlidx on wordlocation(wordid)')
+        self.con.execute('create index urltoidx on link(toid)')
+        self.con.execute('create index urlfromidx on link(fromid)')
+        self.dbcommit()
